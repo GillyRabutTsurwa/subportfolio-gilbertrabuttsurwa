3200795761d3f1778065fd4441c239429f335d5e
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-01-30 03:33:20 -0600

    First commit of subportfolio

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
+
+function copyHTML() {
+    return gulp.src("./src/*.html")
+    .pipe(gulp.dest("./dist"));
+}
+
+function copyImages() {
+    return gulp.src("./src/img/*")
+    .pipe(gulp.dest("./dist/img"));
+}
+
+function copyIcons() {
+    return gulp.src("./src/Icons/*")
+    .pipe(gulp.dest("./dist/Icons"));
+}
+
+function compileSass() {
+    return gulp.src("./src/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./dist/CSS/"))
+    .pipe(browserSync.stream());
+}
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    gulp.watch("./src/*.html", copyHTML);
+    gulp.watch("./src/img/*", copyImages);
+    gulp.watch("./src/Icons/*", copyIcons);
+    gulp.watch("./src/sass/*.scss", compileSass);
+
+    gulp.watch("./src/*.html").on("change", browserSync.reload);
+}
+
+exports.copyHTML = copyHTML;
+exports.copyImages = copyImages;
+exports.copyIcons = copyIcons;
+exports.compileSass = compileSass;
+exports.serve = serve;
