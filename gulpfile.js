67f11170a7b3b7b492bc01ce241960262f4e7720
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-01-30 18:21:23 -0600

    Update to website and minified images

--- gulpfile.js
+++ gulpfile.js
@@ -4,41 +4,39 @@ const cleanCSS = require("gulp-clean-css");
 const browserSync = require("browser-sync").create();
 
 function copyHTML() {
-    return gulp.src("./src/*.html")
-    .pipe(gulp.dest("./dist"));
+	return gulp.src("./src/*.html").pipe(gulp.dest("./dist"));
 }
 
 function copyImages() {
-    return gulp.src("./src/img/*")
-    .pipe(gulp.dest("./dist/img"));
+	return gulp.src("./src/img/*").pipe(gulp.dest("./dist/img"));
 }
 
 function copyIcons() {
-    return gulp.src("./src/Icons/*")
-    .pipe(gulp.dest("./dist/Icons"));
+	return gulp.src("./src/icons/*").pipe(gulp.dest("./dist/icons"));
 }
 
 function compileSass() {
-    return gulp.src("./src/sass/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("./dist/CSS/"))
-    .pipe(browserSync.stream());
+	return gulp
+		.src("./src/sass/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest("./dist/CSS/"))
+		.pipe(browserSync.stream());
 }
 
 function serve() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist"
-        }
-    });
+	browserSync.init({
+		server: {
+			baseDir: "./dist"
+		}
+	});
 
-    gulp.watch("./src/*.html", copyHTML);
-    gulp.watch("./src/img/*", copyImages);
-    gulp.watch("./src/Icons/*", copyIcons);
-    gulp.watch("./src/sass/*.scss", compileSass);
+	gulp.watch("./src/*.html", copyHTML);
+	gulp.watch("./src/img/*", copyImages);
+	gulp.watch("./src/Icons/*", copyIcons);
+	gulp.watch("./src/sass/*.scss", compileSass);
 
-    gulp.watch("./src/*.html").on("change", browserSync.reload);
+	gulp.watch("./src/*.html").on("change", browserSync.reload);
 }
 
 exports.copyHTML = copyHTML;
